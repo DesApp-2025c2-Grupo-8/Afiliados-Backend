07e414c5aa30eecad83a04d22b9d352b32b4775d
SantiagoSolari <santiagosolariivan@gmail.com>
2025-10-15 12:41:31 -0300

    Agregado de insertMany + deleteAll

--- src/recetas/recetas.service.ts
+++ src/recetas/recetas.service.ts
@@ -6,16 +6,24 @@ import { CreateRecetaDto } from './dto/create-receta.dto';
 
 @Injectable()
 export class RecetasService {
-    constructor(@InjectModel(Receta.name) private recetaModel: Model<Receta>){
+    constructor(@InjectModel(Receta.name) private recetaModel: Model<Receta>) {
 
     }
 
-    async findAll(): Promise<Receta[]>{
+    async findAll(): Promise<Receta[]> {
         return this.recetaModel.find().exec();
     }
 
-    async create(recetaACrear: CreateRecetaDto): Promise<Receta>{
+    async deleteAll() : Promise<void> {
+        await this.recetaModel.deleteMany({});
+    }
+
+    async create(recetaACrear: CreateRecetaDto): Promise<Receta> {
         const recetaCreada = new this.recetaModel(recetaACrear);
         return recetaCreada.save();
+    }
+
+    async insertMany(recetas: CreateRecetaDto[]): Promise<Receta[]> {
+        return this.recetaModel.insertMany(recetas).then((docs) => docs.map((d) => d.toObject())); // esto convierte los docs en objetos simples (sin metodos de mongoose)
     }
 }
