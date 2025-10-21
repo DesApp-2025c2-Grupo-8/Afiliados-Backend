07854ceeded05db5e9a03ac06713c82e12ff389e
ValentinVarela <valentin.adriel.varela@gmail.com>
2025-10-21 18:13:52 -0300

    Agregados los Http code status correspondientes en cada petición del recetas controller

--- src/recetas/recetas.controller.ts
+++ src/recetas/recetas.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { 
+    Body,
+    Controller, 
+    Get, 
+    Post, 
+    Param,
+    BadRequestException,
+    NotFoundException,
+    InternalServerErrorException
+} from '@nestjs/common';
 import { RecetasService } from './recetas.service';
 import { CreateRecetaDto } from './dto/create-receta.dto';
 import { Receta } from 'src/schemas/receta.schema';
@@ -15,11 +24,18 @@ export class RecetasController {
 
     @Get('/:numeroAfiliado')
     async findByNumeroAfiliado(@Param('numeroAfiliado') numeroAfiliado: string): Promise<Receta[]> {
-        return this.recetaService.findByNumeroAfiliado(numeroAfiliado);
+        if (!/^\d+$/.test(numeroAfiliado) || numeroAfiliado.length !== 9) throw new BadRequestException('El formato del número de afiliado es inválido');
+        const recetas = await this.recetaService.findByNumeroAfiliado(numeroAfiliado);
+        if (!recetas || recetas.length === 0) throw new NotFoundException('No han sido encontradas recetas para el integrante especificado');
+        return recetas;
     }
 
     @Post()
     async create(@Body() recetaACrear: CreateRecetaDto): Promise<Receta> {
-        return this.recetaService.create(recetaACrear);
+        try {
+            return this.recetaService.create(recetaACrear);
+        } catch (error) {
+            throw new InternalServerErrorException('No ha sido posible crear la receta');
+        }
     }
 }
