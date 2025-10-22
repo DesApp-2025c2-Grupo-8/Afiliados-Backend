82230896d1b464a674a7fd50f09897849cdc3b54
ValentinVarela <valentin.adriel.varela@gmail.com>
2025-10-22 06:30:07 -0300

    Mas errores solucionados

--- src/seeds/reintegros.seed.ts
+++ src/seeds/reintegros.seed.ts
@@ -1,11 +1,11 @@
 import { ReintegrosService } from "src/reintegros/reintegros.service";
 import reintegros from '../data/reintegros'
 
-export async function seedRecetas(recetasService: ReintegrosService) {
+export async function seedReintegros(reintegrosService: ReintegrosService) {
 
-    await recetasService.deleteAll();
+    await reintegrosService.deleteAll();
 
-    await recetasService.insertMany(reintegros);
+    await reintegrosService.insertMany(reintegros);
 
     console.log('reintegros creados: ', reintegros.length)
 }
